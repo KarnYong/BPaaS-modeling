82d1c5b96a7186929b44e97fc82514b90d0a2daa
Karn Yongsiriwit <karnyong@gmail.com>
2015-04-01 13:11:01 +0200

    Remove unnecessary code in StorageFactory

--- platform extensions/bpmn20xmlbasic/src/de/hpi/bpmn2_0/factory/node/StorageFactory.java
+++ platform extensions/bpmn20xmlbasic/src/de/hpi/bpmn2_0/factory/node/StorageFactory.java
@@ -1,7 +1,5 @@
 package de.hpi.bpmn2_0.factory.node;
 
-import javax.xml.namespace.QName;
-
 import org.oryxeditor.server.diagram.generic.GenericShape;
 
 import de.hpi.bpmn2_0.annotations.StencilId;
@@ -9,7 +7,6 @@ import de.hpi.bpmn2_0.exceptions.BpmnConverterException;
 import de.hpi.bpmn2_0.factory.AbstractShapeFactory;
 import de.hpi.bpmn2_0.model.BaseElement;
 import de.hpi.bpmn2_0.model.cloudresource.Storage;
-import de.hpi.bpmn2_0.model.cloudresource.StorageList;
 /**
  * The factory for storage
  * 
@@ -28,8 +25,6 @@ public class StorageFactory extends AbstractShapeFactory {
 			storage.setId(shape.getResourceId());		
 			String capacity = shape.getProperty("capacity");
 			storage.setCapacity(capacity);
-						
-			StorageList.getInstance().storages.put(storage.getId(), storage);
 			
 			return storage;
 		} catch (Exception e) {
